492daaea967b26a91759b981ab510de7a02d2b26
Nispand1492 <nispand.mehta@gmail.com>
2015-09-27 20:46:35 -0500

    requestpattern aded

--- src/com/gontuseries/hellocontroller/HelloController.java
+++ src/com/gontuseries/hellocontroller/HelloController.java
@@ -4,13 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONArray;
+
 import jdk.nashorn.internal.parser.JSONParser;
 
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
@@ -18,10 +24,17 @@ import sun.net.www.protocol.http.HttpURLConnection;
  
 public class HelloController extends AbstractController{
  
-	@Override
+	@RequestMapping(value = "get_data.html" , method = RequestMethod.GET)
+	protected ModelAndView get_data(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		ModelAndView modelandview = new ModelAndView("getdata");
+		modelandview.addObject("welcomeMessage","Enter Location Zip Code of any US city" );
+		return modelandview;
+	}
+	@RequestMapping(value = "welcome.html" , method = RequestMethod.POST)
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
-		String urlStr = "http://api.openweathermap.org/data/2.5/weather?zip=94040,us";
+		HttpServletResponse response ,@RequestParam Map<String,String> reqPar) throws Exception {
+		String zip = reqPar.get("zipcode");
+		String urlStr = "http://api.openweathermap.org/data/2.5/weather?zip="+zip+",us";
 		URL url = new URL(urlStr);
 		System.out.println("Connected");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -53,5 +66,11 @@ public class HelloController extends AbstractController{
 		modelandview.addObject("welcomeMessage",output );
 		
 		return modelandview;
+	}
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
